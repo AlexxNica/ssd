29efd960d60b6f5620e866c99fd75967572c675b
matto <matt@mirabot.coop>
2014-07-12 15:39:56 -0700

    Working permlinks based on clean version of a module's title.

--- docs/sites/all/modules/features/playlist/js/playlist.js
+++ docs/sites/all/modules/features/playlist/js/playlist.js
@@ -1,27 +1,37 @@
-jQuery(document).ready(function($) {
-  var permLink = document.URL.split('#')[1];
-  // @todo Should make sure permLink is in bounds, otherwise set to zero.
-  
-  $('.flexslider').flexslider({
-    animation: "fade",
-    controlNav: false,
-    directionNav: false,
-    slideshow: false,
-    animationSpeed: 0,
-    startAt: permLink,
-    after:function(slider){
-      // Change the hash to a slide's permlink on after sliding.
-      window.location.hash = slider.currentSlide;
-    },
-  });
-  
-  $('.flexslider-prev').on('click', function(){
-    $('.flexslider').flexslider('prev');
-    return false;
-  })
+Drupal.behaviors.playlist = {
+  attach: function (context, settings) {
+    // Get permlink info from Drupal.settings.
+    var hashLinks = Drupal.settings.playlist.hashLinks;
+    var indexes = Drupal.settings.playlist.indexes;
+    (function ($) {
+      
+      // Figure out which slide to load based on hash.
+      var hash = document.URL.split('#')[1];
+      var index = indexes[hash];
 
-  $('.flexslider-next').on('click', function(){
-    $('.flexslider').flexslider('next')
-    return false;
-  });
-});
+      // Load flexslider with default settings.
+      $('.flexslider').flexslider({
+        animation: "fade",
+        controlNav: false,
+        directionNav: false,
+        slideshow: false,
+        animationSpeed: 0,
+        startAt: index,
+        after:function(slider){
+          // Change the hash to a slide's permlink on after sliding.
+          window.location.hash = hashLinks[slider.currentSlide];
+        },
+      });
+      // Flexslider custom controls.
+      $('.flexslider-prev').on('click', function(){
+        $('.flexslider').flexslider('prev');
+        return false;
+      })
+
+      $('.flexslider-next').on('click', function(){
+        $('.flexslider').flexslider('next')
+          return false;
+      });
+    })(jQuery);
+  }
+};
