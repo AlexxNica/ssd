0b1dc0637555246bf246ee388ebb320d843d1677
matto <matt@mirabot.coop>
2014-07-13 00:51:54 -0700

    Added better support for hash-based navigation of playlist.

--- docs/sites/all/modules/features/playlist/js/playlist.js
+++ docs/sites/all/modules/features/playlist/js/playlist.js
@@ -1,13 +1,19 @@
+window.onunload=function(){};
+// @bug @todo https://stackoverflow.com/questions/7248111/how-to-prevent-content-being-displayed-from-back-forward-cache-in-firefox
 Drupal.behaviors.playlist = {
   attach: function (context, settings) {
+    
     // Get permlink info from Drupal.settings.
     var hashLinks = Drupal.settings.playlist.hashLinks;
     var indexes = Drupal.settings.playlist.indexes;
     (function ($) {
-      
+
       // Figure out which slide to load based on hash.
       var hash = document.URL.split('#')[1];
       var index = indexes[hash];
+      if (index == null) {
+        index = 0;
+      }
 
       // Load flexslider with default settings.
       $('.flexslider').flexslider({
@@ -22,16 +28,28 @@ Drupal.behaviors.playlist = {
           window.location.hash = hashLinks[slider.currentSlide];
         },
       });
+      
       // Flexslider custom controls.
       $('.flexslider-prev').on('click', function(){
         $('.flexslider').flexslider('prev');
         return false;
       })
-
       $('.flexslider-next').on('click', function(){
         $('.flexslider').flexslider('next')
           return false;
       });
+
+      // This uses the hashchange plugin to support changing slides when using
+      // the back button. Without it, navigation is unsatisfactory.
+      $(window).hashchange( function(){
+        hash = document.URL.split('#')[1];
+        index = indexes[hash];
+        if (index == null) {
+          index = 0;
+        }
+        $('.flexslider').flexslider(index);
+      });
+
     })(jQuery);
   }
 };
