e2b7911d0d7dc917d51e8f644b028f23c2a86aee
Matt Olenick <matt@mirabot.coop>
2014-11-19 17:01:29 -0800

    5432: Javascript.

--- docs/sites/all/themes/ssd/js/ssd-mobile.js
+++ docs/sites/all/themes/ssd/js/ssd-mobile.js
@@ -1,26 +1,7 @@
 Drupal.behaviors.ssd_mobile = {
   attach: function (context, settings) {
     (function ($) {
-    
-      function mediaqueryresponse(mql){
-        if (mql.matches) {
-          $('.playlist-toc-link').click( function() {
-            $("#playlist-controller .playlist-table", context).hide(100);
-            $('#playlist-controller', context).removeClass('expanded', 100);
-          });
-        }
-        else {
 
-        }
-      }
-      /**
-       * IMPORTANT:
-       * If this is changed, the breakpoint in mobile.scss
-       * must be changed to match.
-       */
-      var mql = window.matchMedia("(max-width:767px)");
-      mediaqueryresponse(mql);
-      mql.addListener(mediaqueryresponse)
     })(jQuery);
   }
 }
--- docs/sites/all/themes/ssd/js/ssd.js
+++ docs/sites/all/themes/ssd/js/ssd.js
@@ -1,6 +1,15 @@
 Drupal.behaviors.ssd_modules = {
   attach: function (context, settings) {
     (function ($) {
+      function mediaqueryresponse(mql){
+        if (mql.matches) {
+          $("#block-views-front-index-modules-block-block .view-grouping-header").show();
+          $('.playlist-toc-link').click( function() {
+            $("#playlist-controller .playlist-table", context).hide(100);
+            $('#playlist-controller', context).removeClass('expanded', 100);
+          });
+        }
+      }
       // Used this example: http://stackoverflow.com/questions/7968303/wrapping-a-series-of-elements-between-two-h2-tags-with-jquery
       $("div.node-article div.wrap-module-sections h2").each(function(){
         var $set = $(this).nextUntil("h2").addBack();
@@ -45,6 +54,65 @@ Drupal.behaviors.ssd_modules = {
           $('#playlist-controller', context).removeClass('expanded', 100);
         }
       });
+      
+      // Frontpage business.
+      function hideAllCategoryChildren() {
+        $(".view-front-index-modules-block").hide(); // @todo: target better
+        $("#category-child-0").hide();
+        $("#category-child-1").hide();
+        $("#category-child-2").hide();
+        $(".view-front-index-block .views-row .views-field-field-module-graphic img").css("background", "#789090");
+        $(".view-front-index-block .views-row .views-field-description-field .field-content").css("color", "#000");
+        $(".view-front-index-block .views-row .views-field-field-module-graphic img").css("opacity", ".5");
+        $(".view-front-index-block .views-row .views-field-description-field .field-content").css("opacity", ".5");
+        $(".view-front-index-block .views-row .views-field-name .field-content").css("opacity", ".5");
+        $(".connect-bar").css("background", "#ddd");
+      }
+      function showCategoryChild(index) {
+        hideAllCategoryChildren();
+        $("#category-parent-" + index + " .views-field-field-module-graphic img").css("background", "#fff");
+        $("#category-parent-" + index + " .views-field-description-field .field-content").css("color", "#fff");
+        $("#category-parent-" + index + " .connect-bar").css("background", "#789090");
+        $("#category-parent-" + index + " views-field-field-module-graphic img").css("opacity", "1");
+        $("#category-parent-" + index + " .views-field-description-field .field-content").css("opacity", "1");
+        $("#category-parent-" + index + " .views-field-name .field-content").css("opacity", "1");
+        $(".view-front-index-modules-block").show();
+        $("#category-child-" + index).show();
+      }
+      $("#category-parent-0").hover(function() {
+        showCategoryChild("0");
+      });
+      $("#category-parent-1").hover(function() {
+        showCategoryChild("1");
+      });
+      $("#category-parent-2").hover(function() {
+        showCategoryChild("2");
+      });
+      $("#block-views-front-index-modules-block-block .view-grouping-header").hide();
+      hideAllCategoryChildren();
+      // Mediaqueries for mobile:
+      /**
+       * IMPORTANT:
+       * If this is changed, the breakpoint in mobile.scss
+       * must be changed to match.
+       */
+      var mql = window.matchMedia("(max-width:767px)");
+      // Runs only on page load.
+      mediaqueryresponse(mql);
+      if (mql.matches) {
+        // @bug Doesn't run in Android/Firefox mobile - why?
+        // On mobile front page, pre-select first icon.
+        $("#category-parent-0 .views-field-field-module-graphic img").css("background", "#fff");
+        $("#category-parent-0 .views-field-description-field .field-content").css("color", "#fff");
+        $("#category-parent-0 .connect-bar").css("background", "#789090");
+        $("#category-parent-0 views-field-field-module-graphic img").css("opacity", "1");
+        $("#category-parent-0 .views-field-description-field .field-content").css("opacity", "1");
+        $("#category-parent-0 .views-field-name .field-content").css("opacity", "1");
+        $(".view-front-index-modules-block").show();
+        $("#category-child-0").show();
+      }
+      // Runs constantly.
+      mql.addListener(mediaqueryresponse);
     })(jQuery);
   }
 }
